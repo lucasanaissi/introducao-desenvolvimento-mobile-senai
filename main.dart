57262fec3b7f1e5e35a8e0fb4ee437370ef45426
Lucas <ljanaissi@gmail.com>
2023-10-23 21:53:21 -0300

    Primeiro commit

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  String nome = 'Lucas';
+
+  int idade = 24;
+
+  if (idade >= 18) {
+    print('$nome é maior de idade');
+  } else {
+    print('$nome é menor de idade');
+  }
+
+
+
+}
