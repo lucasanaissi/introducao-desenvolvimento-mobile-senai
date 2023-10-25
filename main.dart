49703346a17c143aa712cd7f8b7e86796a2c176b
Lucas <ljanaissi@gmail.com>
2023-10-24 21:35:24 -0300

    Commit da aula 24.10

--- main.dart
+++ main.dart
@@ -1,14 +1,28 @@
 void main() {
-  String nome = 'Lucas';
+//    String nome = 'Lucas';
 
-  int idade = 24;
+//    int idade = 11;
 
-  if (idade >= 18) {
-    print('$nome é maior de idade');
+//    if (idade >= 18) {
+//      print('$nome é maior de idade');
+//    } else if (idade < 18 && idade >= 12) {
+//      print('$nome é adolescente');
+//    } else if (idade < 12 && idade > 10) {
+//      print('$nome é pré-adolescente');
+//    } else {
+//      print('$nome é criança');
+// }
+
+String nome = 'Lucas';
+  int idade = 34;
+
+  if (idade <= 12) {
+    print('$nome criança');
+  } else if (idade >= 13 && idade <= 17) {
+    print('$nome adolescente');
+  } else if (idade >= 18 && idade <= 60) {
+    print('$nome adulto');
   } else {
-    print('$nome é menor de idade');
+    print('$nome idoso');
   }
-
-
-
 }
